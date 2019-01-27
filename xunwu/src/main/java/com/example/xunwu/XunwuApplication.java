a950e0e62b1edd6773db4534187cd25b6752d48d
dingyabin <1262027459@qq.com>
2019-01-27 17:26:57 +0800

    xunwu项目

--- xunwu/src/main/java/com/example/xunwu/XunwuApplication.java
+++ xunwu/src/main/java/com/example/xunwu/XunwuApplication.java
@@ -2,12 +2,17 @@ package com.example.xunwu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
-public class XunwuApplication {
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class, scanBasePackages = "com.example")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableTransactionManagement
+public class XunWuApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(XunwuApplication.class, args);
+        SpringApplication.run(XunWuApplication.class, args);
     }
 
 }
