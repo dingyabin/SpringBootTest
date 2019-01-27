73aac7fa0fb7928c95df249a8cb93d0d84f782ab
dingyabin <1262027459@qq.com>
2019-01-27 17:33:11 +0800

    xunwu项目

---
+++ xunwu/src/main/java/com/example/xunwu/config/MybatisConfig.java
@@ -0,0 +1,35 @@
+package com.example.xunwu.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by MrDing
+ * Date: 2018/9/6.
+ * Time:22:13
+ */
+@MapperScan(value = "com.example.xunwu.dao")
+@Configuration
+public class MybatisConfig {
+
+
+    @Bean("dataSource")
+    @ConfigurationProperties("xunwu.datasource")
+    public DataSource dataSource() {
+        return new DruidDataSource();
+    }
+
+
+    @Bean("transactionManager")
+    public DataSourceTransactionManager transactionManager(){
+       return  new DataSourceTransactionManager(dataSource());
+    }
+
+
+}
